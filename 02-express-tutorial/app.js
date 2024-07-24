d5b9555f563b243c9644d6d89dc95456f6731d51
Harshit Rautela <harshitsrautela@gmail.com>
2024-07-24 07:24:09 +0530

    2nd commit

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,13 +1,1 @@
-const http = require('http')
-
-const server = http.createServer((req,res)=>{
-    //res.writeHead() is used to write the HTTP response headers.    
-    //200 is status code telling operation is successful
-    res.writeHead(200,{'content-type':'text/html'})
-    res.write('<h1>Hello</h1>')
-    res.end();
-    
-})
-console.log('Hello')
-
-server.listen(5000)
+console.log("Hello")
